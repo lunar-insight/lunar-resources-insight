491bed1fa2a5776ba33512927f800368138805af
thb-p <20723083+thb-p@users.noreply.github.com>
2023-07-29 19:32:38 +0200

    polyline (edit): change to primitive and arc

--- src/menu/selection/polyline.js
+++ src/menu/selection/polyline.js
@@ -2,6 +2,8 @@ export function drawPolyline(Cesium, viewer, handler, isPolylineDrawing) {
 
     let polyline = null;
     let polylinePositions = [];
+    let tempLine = null;
+    let polylineCollection = new Cesium.PolylineCollection();
 
     isPolylineDrawing = !isPolylineDrawing;
 
@@ -21,22 +23,63 @@ export function drawPolyline(Cesium, viewer, handler, isPolylineDrawing) {
 
                 polylinePositions.push(clonedCartesian);
 
-                if (!polyline) {
-                    polyline = viewer.entities.add({
-                        name: 'Polyline',
-                        polyline: {
-                            positions: new Cesium.CallbackProperty(() => polylinePositions.slice(), false),
-                            width: 3,
-                            material: Cesium.Color.CYAN,
-                        }
+                if (tempLine) {
+                    polylineCollection.remove(tempLine);
+                    tempLine = null;
+                }
+
+                if (polyline) {
+                    polyline.positions = Cesium.PolylinePipeline.generateCartesianArc({
+                        positions: polylinePositions,
                     });
+                } else {
+                    polyline = polylineCollection.add({
+                        positions: [],
+                        width: 3,
+                        material: Cesium.Material.fromType(Cesium.Material.ColorType, {
+                            color: new Cesium.Color(0, 1, 1, 1.0)
+                        }),
+                    });
+                    viewer.scene.primitives.add(polylineCollection);
                 }
             }
         }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
+
+        handler.setInputAction((movement) => {
+            const cartesian = viewer.camera.pickEllipsoid(movement.endPosition, viewer.scene.globe.ellipsoid);
+
+            if (Cesium.defined(cartesian) && polylinePositions.length > 0) {
+                const lastPosition = polylinePositions[polylinePositions.length -1];
+
+                if (tempLine) {
+                    polylineCollection.remove(tempLine);
+                }
+
+                tempLine = polylineCollection.add({
+                    positions: Cesium.PolylinePipeline.generateCartesianArc({
+                        positions: [lastPosition, cartesian],
+                    }),
+                    width: 3,
+                    material: Cesium.Material.fromType(Cesium.Material.ColorType, {
+                        color: new Cesium.Color(0, 1, 1, 0.5)
+                    }),
+                });
+            }
+        }, Cesium.ScreenSpaceEventType.MOUSE_MOVE)
+
     } else {
         handler.removeInputAction(Cesium.ScreenSpaceEventType.LEFT_CLICK);
+        handler.removeInputAction(Cesium.ScreenSpaceEventType.MOUSE_MOVE);
+
+        if (polyline) {
+            polylineCollection.remove(polyline);
+        }
+        if (tempLine) {
+            polylineCollection.remove(tempLine);
+        }
         polyline = null;
         polylinePositions = [];
+        tempLine = null;
     }
 
     return isPolylineDrawing;
