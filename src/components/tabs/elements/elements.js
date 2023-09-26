cd3d3eca63ab30c7450623859eb21b73c9818712
thb-p <20723083+thb-p@users.noreply.github.com>
2023-09-26 21:54:00 +0200

    element (ux): init canva of palette selection

--- src/components/tabs/elements/elements.js
+++ src/components/tabs/elements/elements.js
@@ -26,3 +26,41 @@ overlay.addEventListener('click', function() {
   hidePeriodicTableOverlay();
   elementsTabContent.style.display = "flex";
 })
+
+/*
+  Color palette
+*/
+
+import colorRamp from 'images/color-ramp.png';
+import grayRamp from 'images/gray-ramp.png';
+
+// Gradient container
+
+const gradientContainer = document.querySelector('.element-mapgradient-container__palette-container__selection__gradient');
+const imageGradientContainer = document.createElement('img');
+
+imageGradientContainer.src = colorRamp;
+
+gradientContainer.appendChild(imageGradientContainer);
+
+// Gradient menu selector
+
+function createElementPaletteSelectionMenuItem(imageSrc) {
+  const menuItem = document.createElement('div');
+  menuItem.className = 'element-mapgradient-container__palette-container__menu__item';
+  
+  const imgElement = document.createElement('img');
+  imgElement.src = imageSrc;
+
+  menuItem.appendChild(imgElement);
+
+  return menuItem;
+}
+
+const menuContainer = document.querySelector('.element-mapgradient-container__palette-container__menu');
+
+const images = [colorRamp, grayRamp];
+
+images.forEach(image => {
+  menuContainer.appendChild(createElementPaletteSelectionMenuItem(image));
+});
