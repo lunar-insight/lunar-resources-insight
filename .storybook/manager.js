654b804d35cda98752d8039c045fc81b45d0cdf4
thb-p <20723083+thb-p@users.noreply.github.com>
2024-01-02 02:32:09 +0100

    Change storybook theming

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,6 +1,6 @@
 import { addons } from '@storybook/manager-api';
-import lunarTheme from './lunarTheme';
+import storybookTheme from './storybookTheme';
 
 addons.setConfig({
-  theme: lunarTheme,
+  theme: storybookTheme,
 });
