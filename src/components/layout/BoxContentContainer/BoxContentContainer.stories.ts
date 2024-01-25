2cd1ddb3d274aac336550f5a4b9c348a0e171b2d
thb-p <20723083+thb-p@users.noreply.github.com>
2024-01-25 22:01:29 +0100

    BoxContentContainer testing

--- src/components/layout/BoxContentContainer/BoxContentContainer.stories.ts
+++ src/components/layout/BoxContentContainer/BoxContentContainer.stories.ts
@@ -20,10 +20,19 @@ const meta: Meta = {
   },
   component: BoxContentContainer,
   argTypes: {
-    backgroundColor: { control: 'color' },
     height: { control: { type: 'range', min: 0, max: 1000, step: 1 } },
     width: { control: { type: 'range', min: 0, max: 1000, step: 1 } },
-    flexible: { control: 'boolean', description: 'The "flexible" props can be used directly for the BoxContentContainer to fill 100% width and height of the parent container.'},
+    borderRadius: { control: { type: 'range', min: 0, max: 200, step: 1 } },
+    flexible: { control: 'boolean', description: 'The "flexible" props can be used directly for the BoxContentContainer to fill 100% available parent container space for width and height.'},
+    backgroundColor: { control: 'color' },
+    useGradient: { control: 'boolean' },
+    gradientColor1: { control: 'color' },
+    gradientColor2: { control: 'color' },
+    gradientAngle: { control: { type: 'range', min: 0, max: 360, step: 1 } },
+    useShadow: { control: 'boolean' },
+    shadowValues: { control: 'text' },
+    shadowBlur: { control: { type: 'range', min: 0, max: 100, step: 1 } },
+    shadowColor: { control: 'color' },
   },
 };
 
@@ -31,9 +40,18 @@ export default meta;
 
 export const Default: StoryObj<typeof BoxContentContainer> = {
   args: {
-    backgroundColor: theme.color.secondary,
-    height: 8,
-    width: 8,
+    height: 400,
+    width: 250,
+    borderRadius: 20,
     flexible: false,
+    backgroundColor: theme.color.secondary,
+    useGradient: true,
+    gradientColor1: theme.color.secondary,
+    gradientColor2: theme.color.neutralVariant,
+    gradientAngle: 45,
+    useShadow: true,
+    shadowValues: '0px 0px',
+    shadowBlur: 16,
+    shadowColor: '#000'
   }
 };
--- src/components/layout/BoxContentContainer/BoxContentContainer.tsx
+++ src/components/layout/BoxContentContainer/BoxContentContainer.tsx
@@ -3,30 +3,55 @@ import './BoxContentContainer.scss';
 import { theme } from 'theme';
 
 export interface BoxContentContainerProps {
-  backgroundColor?: string;
   minWidth?: number;
   minHeight?: number;
   height?: number;
   width?: number;
-  className?: string;
+  borderRadius?: number;
   flexible?: boolean;
+  backgroundColor?: string;
+  useGradient?: boolean;
+  gradientColor1?: string;
+  gradientColor2?: string;
+  gradientAngle?: number;
+  useShadow?: boolean;
+  shadowValues?: string;
+  shadowBlur?: number;
+  shadowColor?: string;
+  className?: string;
+  children?: React.ReactNode;
 }
 
 export const BoxContentContainer: React.FC<BoxContentContainerProps> = ({
-  backgroundColor = theme.color.secondary,
-  width = 8,
-  height = 8,
-  className = '',
+  width = 40,
+  height = 20,
+  borderRadius = 20,
   flexible = false,
+  backgroundColor = theme.color.secondary,
+  useGradient = true,
+  gradientColor1 = theme.color.secondary,
+  gradientColor2 = theme.color.neutralVariant,
+  gradientAngle = 45,
+  useShadow = true,
+  shadowValues = '0px 0px',
+  shadowBlur = 2,
+  shadowColor = '#000',
+  className = '', // Allow custom styling for other components
+  children, // Allow other components to be nested inside
 }) => {
   const boxContentContainer: React.CSSProperties = {
     backgroundColor,
     width: flexible ? '100%' : `${width}px`,
     height: flexible ? '100%' : `${height}px`,
+    borderRadius: `${borderRadius}px`,
+    background: useGradient ? `linear-gradient(${gradientAngle}deg, ${gradientColor1}, ${gradientColor2})` : backgroundColor,
+    boxShadow: useShadow ? `${shadowValues} ${shadowBlur}px ${shadowColor}` : 'none',
   }
 
   return (
-    <div className={`box-content-container ${className}`} style={boxContentContainer}></div>
+    <div className={`box-content-container ${className}`} style={boxContentContainer}>
+      {children}
+    </div>
   )
 }
 
