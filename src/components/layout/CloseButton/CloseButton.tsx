a89b329beb6e8d1fbbf3adc1a1c5f62c52308911
thb-p <20723083+thb-p@users.noreply.github.com>
2024-08-24 16:56:27 +0200

    Create close button component

---
+++ src/components/layout/CloseButton/CloseButton.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Button, TooltipTrigger } from 'react-aria-components';
+import './CloseButton.scss';
+import { ButtonTooltip } from '../Tooltip/ButtonTooltip';
+
+interface CloseButtonProps {
+  onPress: () => void;
+  className?: string;
+}
+
+const CloseButton: React.FC<CloseButtonProps> = ({ onPress, className = '' }) => {
+  return (
+    <TooltipTrigger>
+      <Button
+        onPress={onPress}
+        aria-label='close'
+        className={`close-button ${className}`}
+      >
+        <span className='material-symbols-outlined close-button__icon'>
+          close
+        </span>
+      </Button>
+      <ButtonTooltip placement='left'>
+        close
+      </ButtonTooltip>
+    </TooltipTrigger>
+  )
+}
+
+export default CloseButton;
